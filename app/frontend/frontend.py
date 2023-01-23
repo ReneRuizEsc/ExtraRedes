269040800299c8d33dd05421ce772d60258b2eda
Emilio Padilla <padilla.emilio97@gmail.com>
2023-01-22 23:37:04 -0600

    Enrutamiento

--- app/frontend/frontend.py
+++ app/frontend/frontend.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,10 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route("/routing")
 def routing():
     return render_template('enrutamiento.html')
+
+@app.route("/routing_change", methods=['POST'])
+def routing_change():
+    rout_status = request.form['routStatus']
+    return render_template('enrutamiento.html', rout_status=rout_status)
+    
+@app.route("/find")
+def find():
+    #do something
+    return render_template('topologia.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
